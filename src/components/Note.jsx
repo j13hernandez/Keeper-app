5cc0f0874f0f9ba4f80053b7cd1726ed4c7e1ddf
Palma <1hernandezj3@gmail.com>
2022-01-15 20:05:00 -0500

    "Init commit, skeleton code for Note keeper app"

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Note = () => {
+  return (
+    <div className='note'>
+      <h1>Title</h1>
+      <p>This is a note keeper app</p>
+    </div>
+  );
+};
+
+export default Note;
