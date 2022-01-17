ebd2fc31c87dc4c3ae7d071d45ec711176720bf5
Palma <1hernandezj3@gmail.com>
2022-01-17 00:42:24 -0500

    "Add functionality to take items from an array and display them on notes"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
+import notes from '../notes';
+
+const noteMaker = (note) => <Note key={note.key} title={note.title} content={note.content} />;
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(noteMaker)}
       <Footer />
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Note = () => {
+const Note = (props) => {
   return (
     <div className='note'>
-      <h1>Title</h1>
-      <p>This is a note keeper app</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 };
